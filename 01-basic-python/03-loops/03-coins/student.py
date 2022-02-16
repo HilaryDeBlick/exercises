fafed057b774dead39a551852338c515627ec21a
Hilary De Blick <hilary.deblick@student.ucll.be>
2022-02-16 20:19:10 +0100

    finished 03-coins

--- 01-basic-python/03-loops/03-coins/student.py
+++ 01-basic-python/03-loops/03-coins/student.py
@@ -1,1 +1,11 @@
-# Write your code here
+from operator import truediv
+from pickle import TRUE
+
+
+def coins(one, two, five, goal):
+    for x in range(one+1):
+        for y in range(two+1):
+            for z in range(five+1):
+                if x + 2*y + 5*z == goal:
+                    return True
+    return False
