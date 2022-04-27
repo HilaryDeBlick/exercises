d4f0f2f4659a3961d13ac0b8b74849d60ac675b4
Hilary <hilarydeblick@gmail.com>
2022-04-27 15:07:30 +0200

    dictionaries done

--- 01-basic-python/07-dictionaries/01-frequencies/student.py
+++ 01-basic-python/07-dictionaries/01-frequencies/student.py
@@ -1,1 +1,11 @@
-# Write your code here
+def frequencies(xs):
+    freq = {}
+#    alternative method
+#
+#    for x in xs:
+#       freq[x] = freq.get(x, 0) + 1
+    for x in xs:
+        if x not in freq:
+            freq[x] = 0
+        freq[x] += 1
+    return freq
