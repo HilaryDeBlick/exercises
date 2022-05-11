2af1197fcbb50e1a31735bf06d4517d94e9e63bd
Hilary <hilarydeblick@gmail.com>
2022-05-11 14:15:14 +0200

    sets done

--- 01-basic-python/08-sets/02-remove-duplicates/student.py
+++ 01-basic-python/08-sets/02-remove-duplicates/student.py
@@ -1,1 +1,12 @@
-# Write your code here
+import re
+
+
+def remove_duplicates(xs):
+    found = set()
+    result = []
+
+    for x in xs:
+        if x not in found:
+            result.append(x)
+            found.add(x)
+    return result
